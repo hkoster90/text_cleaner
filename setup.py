4115ca5237597623c6d813b5e84d0274f5995867
Hendrik Koester <hkoster90@sapo.pt>
2022-06-19 22:26:03 +0100

    init

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name="text_cleaner",
+    name="text_cleaner_nerdler",
     version="1.0.0",
     description="Clean text from special characters and blanks",
     author="nerdler_pt",
     author_email="no-reply@nerdler.pt",
-    packages=["text_cleaner"],
+    packages=["text_cleaner_nerdler"],
     url="https://www.nerdler.pt",
     license="MIT",
     platforms="any"
